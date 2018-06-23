99fca9aa4303fcb1390c79103bf6bc5fd2a7fa3c
visetty <visalalakkimsetty@gmail.com>
2018-06-22 21:09:38 -0700

    business logic changes done

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,10 @@
 
 var resultArray = [];
 
+ function pingPong(count) {
 
- function pingPong() {
- var userinputOne = $("#userInput").val();
-  for (var i = 1; i <= userinputOne; i = i + 1) {
+ var resultArray = [];
+  for (var i = 1; i <= count; i = i + 1) {
     if (i === NaN) {
       alert("Enter a valid Number:");
     } else if ((i % 3) === 0 && (i % 5) === 0) {
@@ -25,13 +25,13 @@ var resultArray = [];
 //UserInteface logic
 
 $(document).ready(function() {
-  $(".form-inline form").submit(function(event) {
+  $(".form-inline").submit(function(event) {
     event.preventDefault();
-    //console.log(event);
     var userinputOne = $("#userInput").val();
-    var displayArray = pingPong();
-    resultArray.forEach(function(displayArray) {
-      $("#yourresult").append("<li>" + displayArray + "</li>");
+    var displayArray = [];
+    displayArray = pingPong();
+    displayArray.forEach(function(item) {
+      $("#yourresult").append("<li>" + item + "</li>");
     });
   });
 });
